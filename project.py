18f384f5f8ef9b3c9b2e8cf6bc58c1e9d65fd469
vagrant <vagrant@vagrant.vm>
2019-09-18 11:51:01 +0000

    fixed copypaste typo

--- project.py
+++ project.py
@@ -80,20 +80,18 @@ def gconnect():
         response.headers['Content-Type'] = 'application/json'
         return response
     # [Step 11] Check to see if the user is already logged in
-    stored_credentials = login_session.get('credentials')
+    stored_access_token = login_session.get('access_token')
     stored_gplus_id = login_session.get('gplus_id')
-    if stored_credentials is not None and gplus_id == stored_gplus_id:
+    if stored_access_token is not None and gplus_id == stored_gplus_id:
         response = make_response(json.dumps('Current user is already connected', 200))
         response.headers['content-type'] = 'application/json'
     # [Step 12] Store the access token in the session for later use
-    login_session['credentials'] = credentials.to_json()
+    login_session['access_token'] = credentials.access_token
     login_session['gplus_id'] = gplus_id
     # [Step 13] Get User Info
     userinfo_url = "https://googleapis.com/oauth2/v2/userinfo"
-
-    #params = {'access_token':credentials.access_tokenansw, 'alt':'json'}
+    params = {'access_token':credentials.access_token, 'alt':'json'}
     answer = requests.get("https://googleapis.com/oauth2/v2/userinfo", params=params)
-    params = {'access_token': credentials.access_token, 'alt': 'json'}
     data = answer.json()
 
     # [Step 14] Store relevant data from User Info
