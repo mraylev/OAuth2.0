94abdb0dd8dc66b098271e9d5c9341588da4a2f8
vagrant <vagrant@vagrant.vm>
2019-09-18 02:26:35 +0000

    updated databases l2c7 'Updating the User Model' fixed typoos in database_setup_users.py and lotsofmenus_user.py. Created restaurantwithusers.db

--- database_setup_users.py
+++ database_setup_users.py
@@ -18,8 +18,8 @@ class Restaurant(Base):
 
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
-    user_id = Column(integer, ForeignKey('user.id'))
-    user = relationship(user)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
 
 
     @property
@@ -41,8 +41,8 @@ class MenuItem(Base):
     course = Column(String(250))
     restaurant_id = Column(Integer,ForeignKey('restaurant.id'))
     restaurant = relationship(Restaurant)
-    user_id = Column(integer, ForeignKey('user.id'))
-    user = relationship(user)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
 
 
     @property
@@ -58,7 +58,7 @@ class MenuItem(Base):
 
 
 
-engine = create_engine('sqlite///restaurantmenuwithusers.db')
+engine = create_engine('sqlite:///restaurantmenuwithusers.db')
 
 
 Base.metadata.create_all(engine)
